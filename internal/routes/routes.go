74f242a716193d2aacc4638f847fce984ca38e01
Recal <iamrecal10@gmail.com>
2024-12-15 02:42:05 +0700

    create routes and update handler

---
+++ internal/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/steviol/golang-backend-bank-app/internal/delivery/handler"
+)
+
+func SetupRoutes(accountHandler *handler.AccountHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/accounts", accountHandler.CreateAccount)
+	mux.HandleFunc("/accounts/transactions", accountHandler.GetTransactions)
+
+	mux.HandleFunc("/transfer", accountHandler.Transfer)
+
+	mux.HandleFunc("/qr/generate", accountHandler.GenerateQRCode)
+	mux.HandleFunc("/qr/process", accountHandler.ProcessQRPayment)
+
+	return mux
+}
